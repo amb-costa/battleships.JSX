f6f4597e43cfcd026a1560511182d3c5d5592678
amb-costa <ocaranzacosta@gmail.com>
2023-07-25 18:26:15 -0400

    main structure board using bootstrap. functionality should be added to each div

---
+++ src/js/component/board.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+
+const Board = () => {
+  return (
+    <div
+      className="container-fluid border border-secondary border-2"
+      id="board"
+    >
+      <div className="row bg-secondary bg-opacity-50">
+        <div className="col-1">-</div>
+        <div className="col-1">1</div>
+        <div className="col-1">2</div>
+        <div className="col-1">3</div>
+        <div className="col-1">4</div>
+        <div className="col-1">5</div>
+        <div className="col-1">6</div>
+        <div className="col-1">7</div>
+        <div className="col-1">8</div>
+        <div className="col-1">9</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">A</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">B</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">C</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">D</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">E</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">F</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">G</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">H</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+      <div className="row">
+        <div className="col-1 bg-secondary bg-opacity-50">I</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+        <div className="col-1">-</div>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
